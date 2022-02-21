0a9046eee488e7c20725d5facd4e2730dd7cb225
Apoorv Raj Saxena <saxenapoorv2000@gmail.com>
2022-02-21 22:25:32 +0530

    First Commit

---
+++ ValidParentheses.java
@@ -0,0 +1,43 @@
+package AllCodes;
+import java.util.*;
+public class ValidParentheses {
+
+	public boolean isValid(String s) {
+        if(s.length()%2!=0)
+            return false;
+        Stack<Character>st=new Stack<>();
+        
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c=='(' || c=='{' || c=='['){
+                st.push(c);
+            }else{
+                if(c==')'){
+                    if(!st.isEmpty()&&st.peek()=='('){
+                        st.pop();
+                    }else{
+                        return false;
+                    }
+                }
+                if(c=='}'){
+                     if(!st.isEmpty()&&st.peek()=='{'){
+                        st.pop();
+                    }else{
+                        return false;
+                    }
+                }
+                if(c==']'){
+                     if(!st.isEmpty()&&st.peek()=='['){
+                        st.pop();
+                    }else{
+                        return false;
+                    }
+                }
+            }
+        }
+        if(st.isEmpty())
+            return true;
+        
+        return false;
+    }
+}
