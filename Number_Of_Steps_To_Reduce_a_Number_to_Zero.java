c269221fcdab923a4ff11f494f0b9058a00d9907
Apoorv Raj Saxena <saxenapoorv2000@gmail.com>
2022-05-09 22:27:51 +0530

    Update: 73

---
+++ Number_Of_Steps_To_Reduce_a_Number_to_Zero.java
@@ -0,0 +1,16 @@
+package AllCodes;
+
+public class Number_Of_Steps_To_Reduce_a_Number_to_Zero {
+	public int numberOfSteps(int num) {
+        int count=0;
+        while(num!=0){
+            if(num%2==0){
+                num=num/2;
+            }else{
+                num=num-1;
+            }
+            count++;
+        }
+        return count;
+    }
+}
