18bd5124e461b116e24d66d85f4349888c326c8b
Apoorv Raj Saxena <saxenapoorv2000@gmail.com>
2022-06-20 23:10:24 +0530

    Update: 114

---
+++ Short_Encoding_Of_Words.java
@@ -0,0 +1,36 @@
+package AllCodes;
+
+import java.util.*;
+
+public class Short_Encoding_Of_Words {
+	 public int minimumLengthEncoding(String[] words) {
+	        
+	        Set<String>s1=new HashSet<>();
+	        Set<String>s2=new HashSet<>();
+	        
+	        for(String s:words){
+	            s1.add(s);
+	            s2.add(s);
+	        }
+	        
+	        for(String s:s1){
+	            
+	            String curr=s;
+	            
+	            for(int j=0;j<curr.length();j++){
+	                String subString=curr.substring(j+1);
+	                if(s2.contains(subString)){
+	                    s2.remove(subString);
+	                }
+	            }
+	        }
+	        
+	        int ans=0;
+	        
+	        for(String s:s2){
+	            ans+=s.length();
+	        }
+	        
+	        return ans+s2.size();
+	    }
+}
