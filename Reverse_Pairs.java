21838f4fa5fd7b26263c30d9f2b78ad6a5fd30b6
Apoorv Raj Saxena <saxenapoorv2000@gmail.com>
2022-06-23 22:18:53 +0530

    Update: 117

---
+++ Reverse_Pairs.java
@@ -0,0 +1,79 @@
+package AllCodes;
+
+import java.util.ArrayList;
+
+public class Reverse_Pairs {
+	
+	//Optimal Approach
+	public int reversePairs(int[] nums) {
+	       return mergeSort(nums,0,nums.length-1);
+	    }
+	    public int mergeSort(int []arr,int i,int j){
+	        
+	        if(i>=j)
+	            return 0;
+	            int count=0;
+	       
+	            int mid=(i+j)/2;
+	            count+=mergeSort(arr, i, mid);
+	            count+=mergeSort(arr, mid+1, j);
+	            count+=merge(arr, i, mid, j);
+	            
+	        
+	        return count;
+	    }
+	    public int merge(int[]arr,int si,int mid,int ei){
+	        int count=0;
+	        int j=mid+1;
+	        for(int i=si;i<=mid;i++)
+	        {
+	            while(j<=ei && arr[i] > 2*(long)arr[j])
+	                j++;
+	            
+	            count+=j-(mid+1);
+	        }
+	        
+	        ArrayList<Integer> temp = new ArrayList<>(); 
+	        int left = si, right = mid+1;
+	        while(left<=mid && right<=ei){
+	            if(arr[left]<arr[right]){
+	                temp.add(arr[left]);
+	                left++;
+	            }else{
+	                temp.add(arr[right]);
+	                right++;
+	            }
+	        }
+	        
+	        while(left<=mid){
+	            temp.add(arr[left]);
+	            left++;
+	        }
+	        while(right<=ei){
+	            temp.add(arr[right]);
+	            right++;
+	        }
+	        
+	         for(int i = si; i<=ei;i++) {
+	            arr[i] = temp.get(i - si); 
+	        }
+	        return count;
+	    }     
+	
+	
+	
+	//Brute Force
+	 public int reversePairsB(int[] nums) {
+	        int count=0;
+	        for(int i=0;i<nums.length;i++){
+	            for(int j=i+1;j<nums.length;j++){
+	            	long comp=(long)2*nums[j];
+	                if(nums[i]>comp) {
+	                	 count++;
+	                }
+	                   
+	            }
+	        }
+	        return count;
+	    }
+}
