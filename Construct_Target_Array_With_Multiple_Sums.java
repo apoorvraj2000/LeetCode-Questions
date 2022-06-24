8f5056fbace72554920c65f32e58bad09c4214fa
Apoorv Raj Saxena <saxenapoorv2000@gmail.com>
2022-06-24 22:34:06 +0530

    Update: 118

---
+++ Construct_Target_Array_With_Multiple_Sums.java
@@ -0,0 +1,23 @@
+package AllCodes;
+
+import java.util.*;
+
+public class Construct_Target_Array_With_Multiple_Sums {
+	public boolean isPossible(int[] target) {
+        long s = 0;
+        PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+       for (int n : target) {
+             s += n;
+             q.add(n);
+       }          
+    while (s > 1 && q.peek() > s / 2) {
+        int cur = q.poll();
+        s -= cur;
+        if (s <= 1) 
+            return s == 0 ? false : true;
+        q.add(cur % (int)s);
+        s += cur % s;
+    }
+    return s == target.length;
+    }
+}
