9b1aa7a73f0ce8c9f321cf658401e7d23c5aeede
Apoorv Raj Saxena <saxenapoorv2000@gmail.com>
2022-06-29 19:56:43 +0530

    Update: 123

---
+++ Queue_Reconstruction_By_Height.java
@@ -0,0 +1,19 @@
+package AllCodes;
+
+import java.util.*;
+
+public class Queue_Reconstruction_By_Height {
+	public int[][] reconstructQueue(int[][] people) {
+        List<int[] > list=new ArrayList<>(people.length);
+        
+        Arrays.sort(people, (a,b)-> a[0]==b[0] ? a[1]-b[1] : b[0]-a[0]);
+        
+        for(int i=0;i<people.length;i++)
+        {
+            list.add(people[i][1] , people[i]);
+           
+        }
+        
+        return list.toArray(new int[people.length][2]);
+    }
+}
