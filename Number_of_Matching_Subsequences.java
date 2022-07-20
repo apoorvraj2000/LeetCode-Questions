11ddebdd6f16a4de1ed248885517b4fdd3e1d563
Apoor Raj Saxena <saxenapoorv2000@gmail.com>
2022-07-20 22:17:37 +0530

    Update: 138

---
+++ Number_of_Matching_Subsequences.java
@@ -0,0 +1,35 @@
+package AllCodes;
+
+import java.util.*;
+
+public class Number_of_Matching_Subsequences {
+	public int numMatchingSubseq(String s, String[] words) {
+        Map<String,Integer>map=new HashMap<>();
+        for(String st:words){
+            if(map.containsKey(st)){
+                map.put(st,map.get(st)+1);
+            }else{
+                map.put(st,1);
+            }
+        }
+        int ans=0;
+        for(String st:map.keySet()){
+            int i=0;
+            int j=0;
+            
+            while(i<s.length() && j<st.length()){
+                if(s.charAt(i)==st.charAt(j)){
+                    i++;
+                    j++;
+                }
+                else{
+                    i++;
+                }
+            }
+            if(j==st.length()){
+                ans+=map.get(st);
+            }
+        }
+        return ans;
+    }
+}
