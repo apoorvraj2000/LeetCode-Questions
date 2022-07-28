31dfe196b1df6db0f62cc5876461f7d304c77a3c
Apoor Raj Saxena <saxenapoorv2000@gmail.com>
2022-07-28 22:03:45 +0530

    Update: 146

---
+++ Valid_Anagram.java
@@ -0,0 +1,24 @@
+package AllCodes;
+
+public class Valid_Anagram {
+	  public boolean isAnagram(String s, String t) {
+	        
+	        int[] alpha = new int[26];
+	        
+	        for (int i = 0; i < s.length(); i++){
+	             alpha[s.charAt(i) - 'a']++;
+	        }
+	           
+	        for (int i = 0; i < t.length(); i++){
+	            alpha[t.charAt(i) - 'a']--;
+	        }
+	            
+	        for (int i : alpha){
+	            if (i != 0){
+	                return false;
+	            }
+	        }
+	            
+	        return true;
+	    }
+}
