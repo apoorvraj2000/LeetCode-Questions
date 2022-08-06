7d223408bd2e30c51dd66bf388e6c43e3314d216
Apoor Raj Saxena <saxenapoorv2000@gmail.com>
2022-08-06 20:59:35 +0530

    Update: 155

---
+++ Poor_Pigs.java
@@ -0,0 +1,15 @@
+package AllCodes;
+
+public class Poor_Pigs {
+	 public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+         int test = minutesToTest/minutesToDie;
+        
+         int i=0;
+        
+        while(Math.pow(test+1,i)< buckets){
+            i++;
+        }
+        
+        return i;
+    }
+}
