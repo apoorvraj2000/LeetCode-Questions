3b1829e8858ad619913b4772d7643b5263ee58c6
Apoor Raj Saxena <saxenapoorv2000@gmail.com>
2022-08-11 20:30:39 +0530

    Update: 160

---
+++ Maximum_69_Number.java
@@ -0,0 +1,15 @@
+package AllCodes;
+
+public class Maximum_69_Number {
+	 public int maximum69Number (int num) {
+         String s = Integer.toString(num);
+        char[] ch = s.toCharArray();
+        for(int i=0; i< ch.length; i++) {
+                if(ch[i] == '6') {
+                    ch[i]='9';
+                    return Integer.parseInt(String.valueOf(ch));
+                }
+        }
+        return num;
+    }
+}
