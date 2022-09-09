1094409f68395601290e2e18b8e42079f1d295d9
Apoor Raj Saxena <saxenapoorv2000@gmail.com>
2022-09-09 23:07:53 +0530

    Update: 185

---
+++ DistributeCandies.java
@@ -0,0 +1,13 @@
+package AllCodes;
+
+import java.util.*;
+
+public class DistributeCandies {
+	public int distributeCandies(int[] candyType) {
+        Set<Integer> set = new HashSet<>();
+        for(int candy: candyType) {
+            set.add(candy);
+        }
+         return Math.min(candyType.length / 2, set.size());
+    }
+}
